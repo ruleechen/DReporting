83e9b4c822523ecdefd768b4f5d413f40e7813f4
Rulee <rulee@live.cn>
2015-06-27 10:46:41 +0800

    V7

---
+++ DReporting.Web/ReportDatas/Categories.cs
@@ -0,0 +1,3 @@
+﻿namespace DReporting.Web.ReportDatas
+{
+}
---
+++ DReporting.Web/ReportDatas/CategoryDataSource.cs
@@ -0,0 +1,24 @@
+﻿using DReporting.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Web;
+
+namespace DReporting.Web.ReportDatas
+{
+    [Export("dbooking.Categories", typeof(IDataSource))]
+    public class CategoryDataSource : IDataSource
+    {
+        public string DataSourceName
+        {
+            get { return "Categories"; }
+        }
+
+        public object QueryData(NameValueCollection query, bool designTime)
+        {
+            return new Categories();
+        }
+    }
+}
