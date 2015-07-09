95164a7fc06e38829fafae9412261c19bc277af5
Ronglin <ronglin@kooboo.com>
2015-07-09 13:47:45 +0800

    add IReportTemplate

---
+++ DReporting/Core/IReportTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DReporting.Core
+{
+    public delegate void ReportInitEventHandler(object sender, ReportInitEventArgs e);
+
+    public interface IReportTemplate
+    {
+        event ReportInitEventHandler OnInitializeComponent;
+    }
+
+    public class ReportInitEventArgs : EventArgs
+    {
+
+    }
+}
