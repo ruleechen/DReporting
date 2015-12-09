c0738806f56bae93b1582d4a4664cbdb46c5c21b
Ronglin <ronglin@kooboo.com>
2015-12-09 16:36:08 +0800

    investigate and implement grid view data export

--- DReporting.Web/Controllers/GridDemoController.cs
+++ DReporting.Web/Controllers/GridDemoController.cs
@@ -11,6 +11,7 @@ using System.Web.SessionState;
 using DevExpress.Web;
 using DevExpress.Web.Mvc;
 using DevExpress.Web.Mvc.UI;
+using DevExpress.XtraPrinting;
 
 namespace DReporting.Web.Controllers
 {
@@ -25,9 +26,67 @@ namespace DReporting.Web.Controllers
         {
             return PartialView("Grid", QuotesProvider.GetQuotes());
         }
+
+        public ActionResult ExportTo(GridViewExportFormat format)
+        {
+            var setting = GetGridViewSetting();
+            var data = QuotesProvider.GetQuotes();
+
+            if (format == GridViewExportFormat.Pdf)
+            {
+                return GridViewExtension.ExportToPdf(setting, data);
+            }
+
+            if (format == GridViewExportFormat.Rtf)
+            {
+                return GridViewExtension.ExportToRtf(setting, data);
+            }
+
+            if (format == GridViewExportFormat.Xls)
+            {
+                return GridViewExtension.ExportToXls(setting, data, new XlsExportOptionsEx { ExportType = DevExpress.Export.ExportType.WYSIWYG });
+            }
+
+            if (format == GridViewExportFormat.Xlsx)
+            {
+                return GridViewExtension.ExportToXlsx(setting, data, new XlsxExportOptionsEx { ExportType = DevExpress.Export.ExportType.WYSIWYG });
+            }
+
+            if (format == GridViewExportFormat.Csv)
+            {
+                return GridViewExtension.ExportToCsv(setting, data, new CsvExportOptionsEx { ExportType = DevExpress.Export.ExportType.WYSIWYG });
+            }
+
+            return Content(string.Empty);
+        }
+
+        public static GridViewSettings GetGridViewSetting()
+        {
+            var settings = new GridViewSettings();
+
+            settings.Name = "gvDataBinding";
+            settings.CallbackRouteValues = new { Controller = "GridDemo", Action = "Grid" };
+            settings.Width = System.Web.UI.WebControls.Unit.Percentage(100);
+
+            settings.ClientSideEvents.Init = "grid_Init";
+            settings.ClientSideEvents.BeginCallback = "grid_BeginCallback";
+            settings.ClientSideEvents.EndCallback = "grid_EndCallback";
+
+            return settings;
+        }
     }
 
-    public class Quote
+    public enum GridViewExportFormat
+    {
+        None,
+        Pdf,
+        Xls,
+        Xlsx,
+        Rtf,
+        Csv
+    }
+
+    public class QuoteData
     {
         static readonly Random random = new Random();
 
@@ -72,16 +131,16 @@ namespace DReporting.Web.Controllers
             get { return HttpContext.Current.Session; }
         }
 
-        public static List<Quote> GetQuotes()
+        public static List<QuoteData> GetQuotes()
         {
             if (Session["Quotes"] == null)
                 Session["Quotes"] = LoadQuotes();
-            var quotes = (List<Quote>)Session["Quotes"];
+            var quotes = (List<QuoteData>)Session["Quotes"];
             UpdateQuotes(quotes);
             return quotes;
         }
 
-        public static List<Quote> LoadQuotes()
+        public static List<QuoteData> LoadQuotes()
         {
             try
             {
@@ -93,9 +152,9 @@ namespace DReporting.Web.Controllers
             }
         }
 
-        static List<Quote> LoadQuotesFromYahoo()
+        static List<QuoteData> LoadQuotesFromYahoo()
         {
-            var quotes = new List<Quote>();
+            var quotes = new List<QuoteData>();
             var url = string.Format(yahooUrl, string.Join("+", symbolsList));
             var request = HttpWebRequest.Create(url);
             using (var stream = request.GetResponse().GetResponseStream())
@@ -105,7 +164,7 @@ namespace DReporting.Web.Controllers
                     while (!reader.EndOfStream)
                     {
                         var values = reader.ReadLine().Replace("\"", "").Split(new char[] { ',' });
-                        Quote quote = new Quote();
+                        QuoteData quote = new QuoteData();
                         quote.Symbol = values[0].Trim();
                         Decimal value;
                         if (Decimal.TryParse(values[1], out value))
@@ -133,27 +192,27 @@ namespace DReporting.Web.Controllers
             return quotes;
         }
 
-        static List<Quote> GenerateQuotes()
+        static List<QuoteData> GenerateQuotes()
         {
-            return new List<Quote> {
-                new Quote { Symbol = "MSFT", Price = 37.95M, DayMax = 37.95M, DayMin = 36.06M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "INTC", Price = 24.85M, DayMax = 25.80M, DayMin = 24.85M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "CSCO", Price = 22.99M, DayMax = 22.99M, DayMin = 22.64M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "SIRI", Price = 3.71M, DayMax = 3.75M, DayMin = 3.69M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "AAPL", Price = 586.73M, DayMax = 586.73M, DayMin = 540.42M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "HOKU", Price = 0, DayMax = 0, DayMin = 0, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "ORCL", Price = 38.11M, DayMax = 38.52M, DayMin = 37.80M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "AMAT", Price = 17.61M, DayMax = 17.69M, DayMin = 17.47M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "YHOO", Price = 40.80M, DayMax = 40.80M, DayMin = 38.86M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "LVLT", Price = 31.85M, DayMax = 34.25M, DayMin = 31.85M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "DELL", Price = 20.63M, DayMax = 26.64M, DayMin = 14.38M, LastUpdated = DateTime.Now },
-                new Quote { Symbol = "GOOG", Price = 1163.70M, DayMax = 1164M, DayMin = 1151.30M, LastUpdated = DateTime.Now }
+            return new List<QuoteData> {
+                new QuoteData { Symbol = "MSFT", Price = 37.95M, DayMax = 37.95M, DayMin = 36.06M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "INTC", Price = 24.85M, DayMax = 25.80M, DayMin = 24.85M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "CSCO", Price = 22.99M, DayMax = 22.99M, DayMin = 22.64M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "SIRI", Price = 3.71M, DayMax = 3.75M, DayMin = 3.69M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "AAPL", Price = 586.73M, DayMax = 586.73M, DayMin = 540.42M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "HOKU", Price = 0, DayMax = 0, DayMin = 0, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "ORCL", Price = 38.11M, DayMax = 38.52M, DayMin = 37.80M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "AMAT", Price = 17.61M, DayMax = 17.69M, DayMin = 17.47M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "YHOO", Price = 40.80M, DayMax = 40.80M, DayMin = 38.86M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "LVLT", Price = 31.85M, DayMax = 34.25M, DayMin = 31.85M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "DELL", Price = 20.63M, DayMax = 26.64M, DayMin = 14.38M, LastUpdated = DateTime.Now },
+                new QuoteData { Symbol = "GOOG", Price = 1163.70M, DayMax = 1164M, DayMin = 1151.30M, LastUpdated = DateTime.Now }
             };
         }
 
-        private static void UpdateQuotes(List<Quote> quotes)
+        private static void UpdateQuotes(List<QuoteData> quotes)
         {
-            foreach (Quote quote in quotes)
+            foreach (QuoteData quote in quotes)
             {
                 if (random.NextDouble() >= 0.7)
                     quote.Update();
